9038f18cd53cb8700de4344dc2853ccdeb28eae2
tklaxbro <tkatchen7@gmail.com>
2019-03-03 22:45:37 -0500

    Define numbers in terms of their previous

--- lambda.rb
+++ lambda.rb
@@ -1,15 +1,17 @@
-### Number definitions
-ZERO  = -> s { -> z {           z      } }
-ONE   = -> s { -> z {         s[z]     } }
-TWO   = -> s { -> z {       s[s[z]]    } }
-THREE = -> s { -> z {     s[s[s[z]]]   } }
-FIVE  = -> s { -> z { s[s[s[s[s[z]]]]] } }
-
 ### Maths
 POWER     = -> a { -> b { b[a] } }
 # This isn't the expanded out formula, it is the inbetween step = λyx.y((λsz.s(z))yx)
 SUCCESSOR = -> w { -> y { -> x { y[w[y][x]] } } }
 ADD = -> a { -> b { a[SUCCESSOR][b]}}
+
+### Number definitions
+ZERO  = -> s { -> z { z } }
+ONE   = SUCCESSOR[ZERO]
+TWO   = SUCCESSOR[ONE]
+THREE = SUCCESSOR[TWO]
+FOUR  = SUCCESSOR[THREE]
+FIVE  = SUCCESSOR[FOUR]
+
 
 ### Functions to actually view the numbers n stuff
 def viewNumber(proc)
